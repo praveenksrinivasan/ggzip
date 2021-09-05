36a9d8167422a22f896c4cd49d1dcde63aa29188
Praveen Kumar Srinivasan <praveen.srinivasan@zip.co>
2021-09-05 20:21:55 +1000

    updating the base theme

--- R/ggzip.R
+++ R/ggzip.R
@@ -7,7 +7,7 @@ theme_zip <- function() {
 
   # ggplot2::theme_minimal() %+replace%    #replace elements we want to change
 
-    ggplot2::theme(
+    ggplot2::theme_minimal() + ggplot2::theme(
       # grid elements
       # panel.grid.major = element_blank(),
       # panel.grid.minor = element_blank(),
